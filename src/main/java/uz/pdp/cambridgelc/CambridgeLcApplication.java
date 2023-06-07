b7c32bca7220b261db93ab89fa259372e4b8d30e
Bilol665 <bilolsultan665@gmail.com>
2023-06-07 16:49:27 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/cambridgelc/CambridgeLcApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.cambridgelc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambridgeLcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CambridgeLcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/cambridgelc/CambridgeLcApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.cambridgelc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambridgeLcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
