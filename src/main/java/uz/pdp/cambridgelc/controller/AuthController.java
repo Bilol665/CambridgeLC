582d4bf9c22b43083ca79cb3843688185c7b742d
Fazliddin <dilshodovfazliddin@gmail.com>
2023-06-09 17:25:15 +0500

    Authorization Build

--- src/main/java/uz/pdp/cambridgelc/controller/AuthController.java
+++ src/main/java/uz/pdp/cambridgelc/controller/AuthController.java
@@ -18,35 +18,6 @@ import java.util.List;
 public class AuthController {
     private final UserService userService;
 
-    @PostMapping("/addStudent")
-    private ResponseEntity<UserEntity> addStudent(
-            @RequestBody UserCreateDto dto
-    ){
-        return ResponseEntity.ok(userService.saveUser(dto, List.of(UserRole.ROLE_STUDENT)));
-    }
-
-
-    @PostMapping("/addTeacher")
-    private ResponseEntity<UserEntity> addTeacher(
-            @RequestBody UserCreateDto dto
-    ){
-        return ResponseEntity.ok(userService.saveUser(dto, List.of(UserRole.ROLE_TEACHER)));
-    }
-
-
-    @PostMapping("/addAdmin")
-    private ResponseEntity<UserEntity> addAdmin(
-            @RequestBody UserCreateDto dto
-    ){
-        return ResponseEntity.ok(userService.saveUser(dto, List.of(UserRole.ROLE_ADMIN)));
-    }
-
-    @PostMapping("/addSupport")
-    private ResponseEntity<UserEntity> addSupport(
-            @RequestBody UserCreateDto dto
-    ){
-        return ResponseEntity.ok(userService.saveUser(dto, List.of(UserRole.ROLE_SUPPORT)));
-    }
     @GetMapping("/login")
     private ResponseEntity<JwtResponse>login(
             @RequestBody LoginDto loginDto
